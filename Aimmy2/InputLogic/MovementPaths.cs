4d112cbe6c2f3fc3f254f213ed428a8baa2dd623
Lofuuzi <177768000+Lofuuzi@users.noreply.github.com>
2026-01-19 13:05:57 +0800

    Update MovementPaths.cs

--- Aimmy2/InputLogic/MovementPaths.cs
+++ Aimmy2/InputLogic/MovementPaths.cs
@@ -28,7 +28,7 @@ namespace InputLogic
 
             double x = uu * u * start.X + 3 * uu * t * control1.X + 3 * u * tt * control2.X + tt * t * end.X;
             double y = uu * u * start.Y + 3 * uu * t * control1.Y + 3 * u * tt * control2.Y + tt * t * end.Y;
-
+/*
             // anti-shake
             if (start.X < end.X - 1)
             {
@@ -38,7 +38,7 @@ namespace InputLogic
             {
                 x += 1;
             }
-
+*/
             // anti-recoil
             if (start.Y < end.Y + 3)
             {
@@ -46,7 +46,7 @@ namespace InputLogic
             }
             else if(start.Y > end.Y - 3)
             {
-                y -= (ranNum() + 1);
+                y -= ranNum();
             }
 
             return new Point((int)x, (int)y);
