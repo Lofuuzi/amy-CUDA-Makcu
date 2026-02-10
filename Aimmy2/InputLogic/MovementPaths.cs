3499d296a1fc8eeb40bad81f1e8f394fea329fe7
Lofuuzi <177768000+Lofuuzi@users.noreply.github.com>
2026-02-10 22:04:54 +0800

    Update MovementPaths.cs

--- Aimmy2/InputLogic/MovementPaths.cs
+++ Aimmy2/InputLogic/MovementPaths.cs
@@ -6,9 +6,9 @@ namespace InputLogic
     {
         private static readonly int[] permutation = new int[512];
 
-         static int ranNum()
+         /*static int ranNum()
         {
-             /*int baseSense = 1;
+             int baseSense = 1;
             // 1. 判定是否觸發 % 機率，NextDouble() 會產生 0.0 ~ 1.0 之間的數
             if (Random.Shared.NextDouble() < 0.66)
             {
