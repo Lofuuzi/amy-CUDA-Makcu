62f71a179b02966a0937170434ea292916c38dc3
Lofuuzi <177768000+Lofuuzi@users.noreply.github.com>
2026-02-10 18:31:47 +0800

    Update MovementPaths.cs

--- Aimmy2/InputLogic/MovementPaths.cs
+++ Aimmy2/InputLogic/MovementPaths.cs
@@ -8,7 +8,7 @@ namespace InputLogic
 
          static int ranNum()
         {
-             int baseSense = 2;
+             int baseSense = 1;
             // 1. 判定是否觸發 % 機率，NextDouble() 會產生 0.0 ~ 1.0 之間的數
             if (Random.Shared.NextDouble() < 0.66)
             {
@@ -40,11 +40,11 @@ namespace InputLogic
             }
 
             // anti-recoil
-            if (start.Y < end.Y + 3)
+            if (start.Y < end.Y + 1)
             {
                 y += ranNum();
             }
-            else if(start.Y > end.Y - 3)
+            else if(start.Y > end.Y - 1)
             {
                 y -= ranNum();
             }
